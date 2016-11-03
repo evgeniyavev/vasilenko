b8229ac8e7897e902a930efedcf5c2443f3967cf
evgeniyavev <vasilenko.evgeniya1996@gmail.com>
2016-11-04 00:09:58 +0200

    Add files via upload

---
+++ vevcore/VEVCore.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace VEVWebLab;
+header("Content-Type: text/html; charset=utf-8");
+
+class VEVCore {
+
+	public function __construct(){
+		echo "Василенко Евгения Валериевна, группа 5133-1";
+	}
+
+}
+
+?>
